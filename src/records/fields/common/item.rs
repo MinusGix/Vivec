ce697917373c28a814d86b7ec38665626fae63f5
MinusGix <MinusGix@gmail.com>
2020-07-16 09:54:02 -0500

    Add QUAL field

--- src/records/fields/common/item.rs
+++ src/records/fields/common/item.rs
@@ -1,10 +1,11 @@
 // Common item (ALCH, AMMO, etc) fields
 
-use super::{FromField, FromFieldError, GeneralField};
+use super::{write_field_header, FromField, FromFieldError, GeneralField, FIELDH_SIZE};
 use crate::{
-    make_formid_field, make_single_value_field, parse::PResult,
-    records::common::NullTerminatedString,
+    impl_static_data_size, impl_static_type_named, make_formid_field, make_single_value_field,
+    parse::PResult, records::common::NullTerminatedString, util::Writable,
 };
+use std::io::Write;
 
 make_single_value_field!(
     /// Inventory icon filename
@@ -45,3 +46,38 @@ make_formid_field!(
     /// Drop ->SNDR
     ZNAM
 );
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct QUAL {
+    quality: Quality,
+}
+impl_static_type_named!(QUAL, b"QUAL");
+impl_static_data_size!(QUAL, FIELDH_SIZE + Quality::static_data_size());
+impl Writable for QUAL {
+    fn write_to<T>(&self, w: &mut T) -> std::io::Result<()>
+    where
+        T: Write,
+    {
+        write_field_header(self, w)?;
+        self.quality.write_to(w)
+    }
+}
+
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum Quality {
+    Novice = 0,
+    Apprentice = 1,
+    Journeyman = 2,
+    Expert = 3,
+    Master = 4,
+}
+impl_static_data_size!(Quality, u32::static_data_size());
+impl Writable for Quality {
+    fn write_to<T>(&self, w: &mut T) -> std::io::Result<()>
+    where
+        T: Write,
+    {
+        (*self as u32).write_to(w)
+    }
+}
